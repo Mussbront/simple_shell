7bd0879492469b4aae11311fac6b9ac70146f555
codenamejj <jmtagare@gmail.com>
2022-12-16 22:13:16 +0000

    main - with loop - first commit

---
+++ main_shell.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+int main(void)
+{
+	while (true)
+	{
+		
+		char *str;
+		char *split;
+
+		printf("$ ");
+		str = input_string();
+		split = split_line(str);
+
+
+		free(str);
+		free(split);
+	}
+	return (0);
+}
