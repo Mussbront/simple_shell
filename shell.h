cab64fe97d9e2ffbd8790862fd844a982ab06d78
codenamejj <jmtagare@gmail.com>
2022-12-16 22:12:23 +0000

    header file

---
+++ shell.h
@@ -0,0 +1,24 @@
+#ifndef _shell_h
+#define _shell_h
+
+/* LIBRARIES */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+
+/* PROTOTYPES */
+
+char* input_string(void);
+char* split_line(char *string);
+
+
+
+
+#endif
