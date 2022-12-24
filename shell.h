9ebcd575523b816aab5bd11215e69987bbe332d3
Mussbront <vmogendi@gmail.com>
2022-12-23 16:58:36 -0800

    Betty

--- shell.h
+++ shell.h
@@ -3,38 +3,52 @@
 
 /* LIBRARIES */
 
-#include <stdio.h> //for printf
-#include <unistd.h> //for fork, execve
-#include <sys/types.h> //for type pid
-#include <sys/wait.h> //for wait
+#include <stdio.h> /* for printf */
+#include <unistd.h> /* for fork, execve*/
+#include <sys/types.h> /* for type pid */
+#include <sys/wait.h> /* for wait */
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h> //for strtok
-#include <errno.h> //for errno and perror
-#include <sys/stat.h> //for use of stat function
-#include <signal.h> //for signal management
-#include <fcntl.h> //for open files
+#include <string.h> /* for strtok */
+#include <errno.h> /* for errno and perror */
+#include <sys/stat.h> /* for use of stat function */
+#include <signal.h> /* for signal management */
+#include <fcntl.h> /* for open files */
 
 /* MACROS */
 
-#include "macros.h" //for msg help and prompt
+#include "macros.h" /* for msg help and prompt */
 
 /* STRUCTURES */
 
-//struct info - struct for the program's data
+/**
+  * struct info - struct for the program's data
+  * @program_name: the name of the executable
+  * @input_line: pointer to input read for _getline
+  * @alias_list: array of pointers with aliases
+  * @exec_counter: number of excecuted comands
+  * @tokens: pointer to array of tokenized input
+  * @command_name: pointer to the first command typed by the user
+  * @file_descriptor: file descriptor to the input of commands
+  * @env: copy of the environ
+ */
 typedef struct info
 {
-	char *program_name; //the name of the executable
-	char *input_line; //pointer to input read for _getline
-	char **alias_list; //pointer to first command typed by the user
-	int exec_counter; //number of excecuted comands
-	char **tokens; //pointer to array of tokenized input
-	char *command_name; //pointer to the first command typed by the user
-	int file_descriptor; //file descriptor to the input of commands
-	char **env; //copy of the environ
+	char *program_name;
+	char *input_line;
+	char **alias_list;
+	int exec_counter;
+	char **tokens;
+	char *command_name;
+	int file_descriptor;
+	char **env;
 } data_of_program;
 
-//struct builtins - struct for the builtins
+/**
+  * struct builtins - struct for the builtins
+  * @builtin: the name of the builtin
+  * @function: the associated function to be called for each builtin
+  */
 typedef struct builtins
 {
 	char *builtin;
@@ -45,33 +59,33 @@ typedef struct builtins
 
 /* PROTOTYPES */
 
-//Inicialize the struct with the info of the program
+/*Inicialize the struct with the info of the program */
 void inic_data(data_of_program *data, int arc, char *argv[], char **env);
-//Makes the infinite loop that shows the prompt
+/* Makes the infinite loop that shows the prompt */
 void prompt(char *prompt, data_of_program *data);
-//Print the prompt in a new line
+/*Print the prompt in a new line */
 void ctrl_c(int opr UNUSED);
-//Read one line of the standar input
+/* Read one line of the standar input */
 int _getline(data_of_program *data);
-//split each line for logical operators if they exist
+/* split each line for logical operators if they exist */
 int logic_ops(char *array_commands[], int i, char array_operators[]);
-//expand variables
+/* expand variables */
 void expand_variables(data_of_program *data);
-//expand aliases
+/*expand aliases */
 void expand_alias(data_of_program *data);
-//append the string to the end of the buffer
+/* append the string to the end of the buffer */
 int buffer_add(char *buffer, char *str_to_add);
-//Separate the string in tokens using a designed delimiter
+/* Separate the string in tokens using a designed delimiter */
 void tokenize(data_of_program *data);
-//Create a pointer to part of a string
+/* Create a pointer to part of a string */
 char *_strtok(char *line, char *delim);
-//Execute a command with its entire path
+/* Execute a command with its entire path */
 int exec(data_of_program *data);
-//executes builtin if matched
+/* executes builtin if matched */
 int builtins_list(data_of_program *data);
-//Creates aaray of path directories
+/* Creates aaray of path directories */
 char **tokenize_path(data_of_program *data);
-//Search for program in path
+/* Search for program in path */
 int find_program(data_of_program *data);
 
 #endif
