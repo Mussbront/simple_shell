8182ee6c2b6bb1175b74b9cdb9f492691be05bcc
Mussbront <vmogendi@gmail.com>
2022-12-23 18:02:10 -0800

    exec.c

--- exec.c
+++ exec.c
@@ -1,32 +1,47 @@
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include "shell.h"
-
 /**
-  * _exec - Executes program
-  * @args: arguments to be passed
+  * exec - Executes command
+  * @data: a pointer to the program's data
   * Return: Success (0)
   */
-
-void _exec(char **args)
+void exec(data_of_program *data)
 {
-	int status;
-	pid_t pid = fork();
+	int retval = 0, status;
+	pid_t pid;
 
-	if (pid == 0)
-	{
-		if ((execve(args[0], args, NULL)) < 0)
-		{
-			perror("\nCould not fork");
-			exit(1);
-		}
+	/* check for program in built ins */
+	retval = builtins_list(data);
+	if (retval != -1)/* if program was found in built ins */
+		return (retval);
+	
+	/* check for program file system */
+	retval = find_program(data);
+	if (retval)
+	{/* if program not found */
+		return (retval);
 	}
 	else
-	{
-		do waitpid(pid, &status, 0);
-		while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	{/* if program was found */
+		pidd = fork(); /* create a child process */
+		if (pidd == -1)
+		{/* if the fork call failed */
+			perror(data->command_name);
+			exit(EXIT_FAILURE);
+		}
+		if (pidd == 0)
+		{/* I am the child process, I execute the program*/
+			retval = execve(data->tokens[0], data->tokens, data->env);
+			if (retval == -1) /* if error when execve*/
+				perror(data->command_name), exit(EXIT_FAILURE);
+		}
+		else 
+		{/* This is the parent */
+			wait(&status);
+			if (WIFEXITED(status))
+				errno = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				errno = 128 + WTERMSIG(status);
+		}
 	}
+	return (0);
 }
